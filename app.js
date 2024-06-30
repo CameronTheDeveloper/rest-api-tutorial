8b9817932d0294fdcc4c60097667da6d95e725e7
Cameron Posey <cameronposey@outlook.com>
2024-06-30 04:27:48 -0700

    Move app.js data variables to models folder

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+import models from './models';
 
 const createError = require("http-errors");
 const express = require("express");
@@ -52,44 +53,27 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1],
+  };
   next();
 });
 
 app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
 
 app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
+  return res.send(req.context.models.users[req.params.userId]);
 });
-
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
-});
-
-
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-
-
 
 app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
 
 app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
 });
 
 app.post('/messages', (req, res) => {
@@ -97,10 +81,21 @@ app.post('/messages', (req, res) => {
   const message = {
     id,
     text: req.body.text,
-    userId: req.me.id,
+    userId: req.context.me.id,
   };
 
-  messages[id] = message;
+  req.context.models.messages[id] = message;
+
+  return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = req.context.models.messages;
+
+  req.context.models.messages = otherMessages;
 
   return res.send(message);
 });
@@ -117,7 +112,7 @@ app.delete('/messages/:messageId', (req, res) => {
 });
 
 app.get('/session', (req, res) => {
-  return res.send(users[req.me.id]);
+  return res.send(req.context.models.users[req.context.me.id]);
 });
 
 
